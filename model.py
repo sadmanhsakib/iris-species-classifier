c2a9a266c1c90e9f6ec10442eef27cd32ca7c59f
Sadman H. Sakib <831.sakib@gmail.com>
2026-04-28 23:59:02 +0600

    feat: added random forest classification model for comparing

--- model.py
+++ model.py
@@ -1,40 +1,91 @@
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
 
 iris = load_iris()
 
 X = iris.data
 y = iris.target
 
-# spling the data
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=0.2, # reserving 20% of the data for testing
-    random_state=42, # seed for reproducibility
-    stratify=y # preserve class balance in both splits
-)
 
-pipeline = Pipeline(
-    steps=[("scaler", StandardScaler()), ("model", LogisticRegression(max_iter=200))]
-)
+def main():
+    X_train, X_test, y_train, y_test = split_data()
 
-pipeline.fit(X_train, y_train)
+    lr_f1, lr_train_score, lr_test_score = logistic_classification(X_train, X_test, y_train, y_test)
+    rf_f1, rf_train_score, rf_test_score = random_forest_classification(X_train, X_test, y_train, y_test)
 
-# model performance on seen data 
-train_score = pipeline.score(X_train, y_train)
-# model performance on unseen data 
-test_score = pipeline.score(X_test, y_test)
+    print(f"{lr_f1}, {rf_f1}")
+    print(f"{lr_test_score}, {rf_test_score}")
+    print(f"{lr_train_score}, {rf_train_score}")
 
-print(f"Score(seen-data): {round(train_score*100, 2)}%")
-print(f"Score(unseen-data): {round(test_score*100, 2)}%")
+def split_data():
+    # spliting the data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,  # reserving 20% of the data for testing
+        random_state=42,  # seed for reproducibility
+        stratify=y,  # preserve class balance in both splits
+    )
+    return X_train, X_test, y_train, y_test
 
-sample = [[5.1, 3.5, 1.4, 0.2]]  # A single new observation
 
-prediction = pipeline.predict(sample)
-predicted_species = iris.target_names[prediction[0]]
+def logistic_classification(
+    X_train: np.ndarray, X_test: np.ndarray, y_train: np.ndarray, y_test: np.ndarray
+) -> tuple[float, float, float]:
+    # LogisticRegression model (does classification)
+    lr_pipeline = Pipeline(
+        steps=[
+            ("scaler", StandardScaler()),
+            ("model", LogisticRegression(max_iter=200)),
+        ]
+    )
 
-print(f"Predicted species: {predicted_species}")  # setosa
+    # training the model with training data
+    lr_pipeline.fit(X_train, y_train)
+
+    # model performance on seen data
+    train_score = lr_pipeline.score(X_train, y_train)
+    # model performance on unseen data
+    test_score = lr_pipeline.score(X_test, y_test)
+    f1 = f1_score(y_test, lr_pipeline.predict(X_test), average="weighted")
+
+    f1 = round(f1*100, 2)
+    train_score = round(train_score*100, 2)
+    test_score = round(test_score*100, 2)
+
+    return f1, train_score, test_score
+
+
+def random_forest_classification(X_train: np.ndarray, X_test: np.ndarray, y_train: np.ndarray, y_test: np.ndarray
+) -> tuple[float, float, float]:
+    # random forest pipeline
+    rf_pipeline = Pipeline(
+        steps=[
+            ("scaler", StandardScaler()),
+            ("model", RandomForestClassifier(n_estimators=100, random_state=42)),
+        ]
+    )
+
+    # training the model with training data
+    rf_pipeline.fit(X_train, y_train)
+
+    # model performance on seen data
+    train_score = rf_pipeline.score(X_train, y_train)
+    # model performance on unseen data
+    test_score = rf_pipeline.score(X_test, y_test)
+    f1 = f1_score(y_test, rf_pipeline.predict(X_test), average="weighted")
+
+    f1 = round(f1 * 100, 2)
+    train_score = round(train_score * 100, 2)
+    test_score = round(test_score * 100, 2)
+
+    return f1, train_score, test_score
+
+
+main()
